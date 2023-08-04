1f99e33f204daa439c3d1fa99a38034a6334f705
Damon sarceno <Damonsarceno@gmail.com>
2023-08-04 14:20:07 -0400

    done

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+    const rev = word.split("").reverse().join("");
+  //compare the reveresd string to the input
+    if (rev == word) {
+      return true
+     }
+      return false;
 }
 
 /* 
-  Add your pseudocode here
+  reverse the input string and if the reversed string is the same as the input then return true
+  if else return false
 */
 
 /*
   Add written explanation of your solution here
+  in this lab i need to find a solution that will work properly on palindrome's 
+  giving a true or false answer. I found the easiest way for this problem was to 
+  use the inbuilt string methods split() , reverse() and join() to get the correct answer.
+  this method takes a string and splits it into a character array then reveresd the array
+  and finally joined them back into a string.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+  
+  console.log("");
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("hannah"));
+  
+  console.log("");
+  
+  console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+  
+  console.log("");
+  
+  console.log("=>", isPalindrome("robot"));
+  console.log("Expecting: false");
+  
+  console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("ab"));
+
+  console.log("=>", isPalindrome("damon"));
+  console.log("Expecting: false");
+  
+  console.log("");
+
 }
 
 module.exports = isPalindrome;
